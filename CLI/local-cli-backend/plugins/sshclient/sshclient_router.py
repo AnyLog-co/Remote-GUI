4e29c9ef15ea49aea373061939302bb54ef08850
Parth Khanna <khanna.parth@outlook.com>
2026-03-18 18:31:54 -0700

    Added backend docker fixes to node naming

--- CLI/local-cli-backend/plugins/sshclient/sshclient_router.py
+++ CLI/local-cli-backend/plugins/sshclient/sshclient_router.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, WebSocket, WebSocketDisconnect
-import io
 import asyncio
+import io
+
 import paramiko
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 
 # Backend-required 'api_router' object to consume SSHClient's router into main router
 api_router = APIRouter(prefix="/sshclient", tags=["SSH Client"])
@@ -9,10 +10,11 @@ api_router = APIRouter(prefix="/sshclient", tags=["SSH Client"])
 # Allowed on-start tasks
 ALLOWED_ON_START_TASKS = ["direct_ssh", "docker_attach", "docker_exec"]
 # Possible allowed conn method.
-ALLOWED_CONNECTION_METHODS = ["password","key-string","keyfile"]
+ALLOWED_CONNECTION_METHODS = ["password", "key-string", "keyfile"]
 
 # Global mapping of all active sessions.
 sessions = {}
+
 
 async def listen_to_ssh(ws: WebSocket, channel: paramiko.Channel):
     """
@@ -21,14 +23,15 @@ async def listen_to_ssh(ws: WebSocket, channel: paramiko.Channel):
     try:
         while True:
             if channel.recv_ready():
-                output = channel.recv(4096).decode('utf-8', 'replace')
+                output = channel.recv(4096).decode("utf-8", "replace")
                 await ws.send_text(output)
             # Avoid wasting possible sleep cycles
             await asyncio.sleep(0.02)
     except Exception:
         pass
 
-def connect_client(host, user, password= None, pkey = None) -> paramiko.SSHClient:
+
+def connect_client(ip, user, password=None, pkey=None) -> paramiko.SSHClient:
     """
     Inputs hostname, user, password, or possible p-key and returns a Paramiko-initialized Client
     """
@@ -37,14 +40,13 @@ def connect_client(host, user, password= None, pkey = None) -> paramiko.SSHClien
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         client.connect(
-            hostname=host,
+            hostname=ip,
             username=user,
             port=22,
             password=password,
             pkey=pkey,
             timeout=10,
         )
-        
 
         return client
 
@@ -54,9 +56,9 @@ def connect_client(host, user, password= None, pkey = None) -> paramiko.SSHClien
     except Exception as e:
         print("Error creating Paramiko Client:", e)
         return None
-    
 
-def open_ssh_chan(host, user, conn_method):
+
+def open_ssh_chan(ip, name, user, conn_method):
     """
     Requires a hostname, user, and conn_method object
     conn_method in form of a dictionary:
@@ -66,28 +68,26 @@ def open_ssh_chan(host, user, conn_method):
         }
         method: "password" or "key-string" or "keyfile"
         data: <SSH_PASSWORD> or <SSH_KEYDATA>
-    """ 
-    pref_method = conn_method.get('method')
-    method_data = conn_method.get('data')
-    print('Method: ', pref_method)
+    """
+    pref_method = conn_method.get("method")
+    method_data = conn_method.get("data")
+    print("Method: ", pref_method)
 
     if pref_method not in ALLOWED_CONNECTION_METHODS:
-        print('Connection Method Error: Not key or password.\n')
+        print("Connection Method Error: Not key or password.\n")
         return None
-    
+
     match pref_method:
         # Password
-        case 'password':
-            print('Connecting via password...')
-            client = connect_client(host, user, password=method_data)
+        case "password":
+            print("Connecting via password...")
+            client = connect_client(ip, user, password=method_data)
 
-
-        case 'key-string':
+        case "key-string":
             # Handle key file data as string
-            print('Connecting via Key [String]...')
+            print("Connecting via Key [String]...")
 
             key_stream = io.StringIO(method_data)
-
 
             # Returns paramiko key object to be used for ssh auth.
             try:
@@ -98,11 +98,11 @@ def open_ssh_chan(host, user, conn_method):
                 key_stream.seek(0)
                 key = paramiko.RSAKey.from_private_key(key_stream)
 
-            client = connect_client(host, user, pkey=key)
+            client = connect_client(ip, user, pkey=key)
 
-        case 'keyfile':
+        case "keyfile":
             # Handle raw keyfile data buffer
-            print('Connecting via Key [File]...')
+            print("Connecting via Key [File]...")
 
             try:
                 # Handle default Ed25519Key key type
@@ -111,39 +111,40 @@ def open_ssh_chan(host, user, conn_method):
                 # Default to RSA key
                 key = paramiko.RSAKey.from_private_key_file(method_data)
 
-            client = connect_client(host, user, pkey=key)
-
+            client = connect_client(ip, user, pkey=key)
 
     return client
+
 
 @api_router.websocket("/ws")
 async def ws_handler(ws: WebSocket):
     # WebSocket endpoint to handle SSHClient logic
     await ws.accept()
-    
+
     channel = None
 
     try:
         while True:
             message = await ws.receive_json()
-            action = message.get("action")
-            conn_method_info = message.get('conn_method')
 
+            node_name = message.get("name")
+            action = message.get("action")
+            conn_method_info = message.get("conn_method")
 
             if action in ALLOWED_ON_START_TASKS:
                 cols = message.get("cols", 80)
                 rows = message.get("rows", 24)
-                
+
                 client = open_ssh_chan(
-                    message["host"],
+                    message["ip"],
+                    node_name,
                     message["user"],
                     conn_method_info,
                 )
 
-                
                 if not client:
-                    print('Unauthorized User.')
-                    await ws.close(code=1008, reason='Unauthorized User')
+                    print("Unauthorized User.")
+                    await ws.close(code=1008, reason="Unauthorized User")
                     return None
 
                 if action == "direct_ssh":
@@ -158,23 +159,22 @@ async def ws_handler(ws: WebSocket):
 
                     if action == "docker_attach":
                         # Create shell and launch docker attach to node on-start
-                        channel.exec_command("docker attach anylog-query1")
+                        channel.exec_command(f"docker attach {node_name}")
 
-                        # Relay shell ready status 
-                        await ws.send_text("Attached to anylog-query1. Press <ctrl>p and then <ctrl>q to detach\r\n")
+                        # Relay shell ready status
+                        await ws.send_text(
+                            f"Attached to anylog-{node_name}. Press <ctrl>p and then <ctrl>q to detach\r\n"
+                        )
 
                     if action == "docker_exec":
                         # Create shell and launch docker exec to node on-start
-                        channel.exec_command("docker exec -it anylog-query1 sh")
+                        channel.exec_command(f"docker exec -it {node_name} sh")
 
                         # Relay shell ready status
-                        await ws.send_text("Started in anylog-query1\r\n")
+                        await ws.send_text(f"Started in {node_name}\r\n")
 
                 # Store connection within global sessions
-                sessions[ws] = {
-                    "client": client,
-                    "channel": channel
-                }
+                sessions[ws] = {"client": client, "channel": channel}
 
                 # Start SSH loop task
                 asyncio.create_task(listen_to_ssh(ws, channel))
@@ -190,9 +190,9 @@ async def ws_handler(ws: WebSocket):
                 if channel:
                     channel.send(message.get("input", ""))
     except WebSocketDisconnect:
-        print('WS Disconnect\n')
+        print("WS Disconnect\n")
     except Exception as e:
-        # Try to return error to user's WebSocket and close 
+        # Try to return error to user's WebSocket and close
         if str(e).lower() == "socket is closed":
             await ws.send_text("Internal connection to remote host broken")
         else:
