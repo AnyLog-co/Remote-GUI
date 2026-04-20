6e077c757054733acaa593efd7fad243eb95ac9f
abell-6103 <andrewbell4829@gmail.com>
2026-04-20 11:38:58 -0700

    refactored _parse by moving some functionality to helper methods

--- CLI/local-cli-backend/plugins/fileuploader/pathparser.py
+++ CLI/local-cli-backend/plugins/fileuploader/pathparser.py
@@ -1,26 +1,23 @@
 from typing import List
 
-def _parse(path: str, dest: List[str]) -> None:
-    dest.clear()
-
-    # Clear leading symbols
-    if len(path) <= 0:
-        return
-    if path[0] == '.':
-        path = path[1:]
-    if len(path) <= 0:
-        return
-    if path[0] == '/' or path[0] == '\\':
-        path = path[1:]
-    if len(path) <= 0:
-        return
-    
+def _get_parsed_elements(path: str) -> List[str] | None:
+    '''
+    Splits the elements of a path into an ordered list. If the path does not
+    follow Unix or Windows path name conventions, returns None.
+    '''
     if len(path.split('/')) > 1:
-        parsed_elements = path.split('/')
+        # Unix convention
+        return path.split('/')
     elif len(path.split('\\')) > 1:
-        parsed_elements = path.split('\\')
-    else:
-        return
+        # Windows convention
+        return path.split('\\')
+    return None
+
+def _alert_to_forbidden_elements(dest: List[str], parsed_elements: List[str]) -> None:
+    '''
+    Checks that the parsed path elements do not violate any rules for paths
+    used by the file uploader. Raises ValueError if a violation is found.
+    '''
 
     # we want this file upload system to upload only through the /app top directory
     if parsed_elements[0] != 'app':
@@ -32,6 +29,13 @@ def _parse(path: str, dest: List[str]) -> None:
         dest.clear()
         raise ValueError("File path cannot contain '..'")
 
+def _handle_path_elements(dest: List[str], parsed_elements: List[str]):
+    '''
+    Makes sure that the parsed path elements have proper naming conventions,
+    i.e. the only element with a file extension is the last element and that
+    that element only has one extension. If everything is fine, the elements
+    are appended to dest. If an issue is found, raises ValueError.
+    '''
     found_ext = False
     for element in parsed_elements:
         if found_ext:
@@ -47,6 +51,28 @@ def _parse(path: str, dest: List[str]) -> None:
                 raise ValueError("File must have name before extension")
             found_ext = True
         dest.append(element)
+
+def _parse(path: str, dest: List[str]) -> None:
+    dest.clear()
+
+    # Clear leading symbols
+    if len(path) <= 0:
+        return None
+    if path[0] == '.':
+        path = path[1:]
+    if len(path) <= 0:
+        return None
+    if path[0] == '/' or path[0] == '\\':
+        path = path[1:]
+    if len(path) <= 0:
+        return None
+    
+    parsed_elements = _get_parsed_elements(path)
+    if parsed_elements is None:
+        return None
+
+    _alert_to_forbidden_elements(dest, parsed_elements)
+    _handle_path_elements(dest, parsed_elements)
 
 class PathAppendException(ValueError):
     pass
