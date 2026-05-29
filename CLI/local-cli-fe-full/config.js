58341a66834bdc09797285ab6c74db45d0c5ce96
royshadmon <16313057+royshadmon@users.noreply.github.com>
2026-05-29 15:24:53 -0700

    resetting the config.json

--- CLI/local-cli-fe-full/config.js
+++ CLI/local-cli-fe-full/config.js
@@ -1,3 +1,7 @@
+// window._env_ = {
+//   VITE_API_URL: "http://localhost:8000"
+// };
+
 window._env_ = {
-  VITE_API_URL: "http://localhost:8000"
+  VITE_API_URL: `http://${window.location.hostname}:8080`
 };
